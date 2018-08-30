82a756315f392db8077da02e6c89521d0049b8e1
Ehi <ehiagheaigg@gmail.com>
2018-08-30 13:50:28 +0100

    Initial express server with route files

---
+++ routes/api/posts.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+//@route GET api/posts/test
+//@desc gets all tests
+//@acccess is public
+router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
+
+module.exports = router;
